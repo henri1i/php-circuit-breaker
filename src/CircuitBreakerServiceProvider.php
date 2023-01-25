3cee5fa42ba7a15a46b4d8faca5271779a3fb3be
henri <henri1i.dev@gmail.com>
2023-01-25 16:43:30 -0300

    initial commit

---
+++ src/CircuitBreakerServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Henri1i\CircuitBreaker;
+
+use Illuminate\Support\;
+
+class CircuitBreakerServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+
+    }
+
+    public function register()
+    {
+
+    }
+}
